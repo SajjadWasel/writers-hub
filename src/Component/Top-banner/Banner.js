9d90bcc615194319a5f3545d9384e697c662ef98
Sajjad Wasel <86624477+SajjadWasel@users.noreply.github.com>
2024-04-17 10:56:46 +0600

    Update Banner.js

--- src/Component/Top-banner/Banner.js
+++ src/Component/Top-banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
         <div className="banner-body">
             <div className="banner">
                 <h1>Welcome To The Writer's World</h1> 
-                <h2>Invite World's greatest writers for your next concert</h2>
+                <h2>Invite World's greatest writers for your next Event</h2>
                 {/* -----Total Budget------ */}
                 <h3 className='mb-3'>Total Budget: 100$ Million</h3>
             </div>
